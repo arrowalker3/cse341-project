2a56d57421c6075d7e45e612ee6bc14788e8ac73
arrowalker3 <arrowalker3@gmail.com>
2021-09-21 19:19:22 -0600

    team activity 2

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,44 @@
 const express = require('express');
 const router = express.Router();
 
+const users = ['user1', 'user2', 'user3'];
+let duplicateName = false;
+let nonexistant = false;
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
     path: '/ta02', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
+    users: users,
+    duplicateName: duplicateName,
+    nonexistant: nonexistant
   });
+});
+
+router.post('/addUser', (req, res, next) => {
+  const newName = req.body.newName;
+  if (users.includes(newName)) {
+    duplicateName = true;
+  } else {
+    users.push(req.body.newName);
+    duplicateName = false;
+    nonexistant = false;
+  }
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const toRemove = req.body.toRemove;
+  const index = users.indexOf(toRemove);
+
+  if (index !== -1) {
+    users.splice(index, 1);
+    duplicateName = false;
+    nonexistant = false;
+  } else {
+    nonexistant = true;
+  }
+  res.redirect('/ta02');
 });
 
 module.exports = router;
